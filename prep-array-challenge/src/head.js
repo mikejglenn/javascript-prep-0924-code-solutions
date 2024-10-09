f4172ace570d7d946d5849d5236d0aed1a3cb6d6
Mike Glenn <mjglenn+lf@gmail.com>
2024-10-09 18:10:16 +0000

    Completed prep-array-challenge exercise.

---
+++ prep-array-challenge/src/head.js
@@ -0,0 +1,42 @@
+/* exported head */
+function head(array) {
+  return array[0];
+}
+
+function last(array) {
+  return array[array.length - 1];
+}
+
+function tail(array) {
+  const newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+function initial(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+function reverse(array) {
+  const newArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+function compact(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
